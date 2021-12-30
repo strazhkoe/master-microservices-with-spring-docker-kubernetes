a16b5293a045e1dc925198e9dbcdc69d7dc0c745
Yevhen Strazhko <strazhkoe.dev@gmail.com>
2021-12-30 10:53:39 +0200

    Changed accounts ms to get props from config server

--- bank application/accounts/src/main/java/com/easybytes/accounts/controller/AccountsController.java
+++ bank application/accounts/src/main/java/com/easybytes/accounts/controller/AccountsController.java
@@ -1,19 +1,28 @@
 package com.easybytes.accounts.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.easybytes.accounts.config.AccountsServiceConfig;
 import com.easybytes.accounts.model.Accounts;
 import com.easybytes.accounts.model.Customer;
+import com.easybytes.accounts.model.Properties;
 import com.easybytes.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 public class AccountsController {
 	
 	@Autowired
 	private AccountsRepository accountsRepository;
+	
+	@Autowired
+	private AccountsServiceConfig accountsConfig;
 	
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -23,6 +32,15 @@ public class AccountsController {
 		} else {
 			return null;
 		}
+	}
+	
+	@GetMapping("/account/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+				accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+		String result = ow.writeValueAsString(properties);
+		return result;
 	}
 
 }
