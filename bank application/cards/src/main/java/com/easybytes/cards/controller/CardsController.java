c9a150053eb778e30332d859840b7cae82f9e1d6
Yevhen Strazhko <strazhkoe.dev@gmail.com>
2022-01-01 14:12:50 +0200

    Changed cards ms to get props from config server

--- bank application/cards/src/main/java/com/easybytes/cards/controller/CardsController.java
+++ bank application/cards/src/main/java/com/easybytes/cards/controller/CardsController.java
@@ -3,13 +3,19 @@ package com.easybytes.cards.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.easybytes.cards.model.Properties;
+import com.easybytes.cards.config.CardsServiceConfig;
 import com.easybytes.cards.model.Cards;
 import com.easybytes.cards.model.Customer;
 import com.easybytes.cards.repository.CardsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 public class CardsController {
@@ -17,10 +23,22 @@ public class CardsController {
 	@Autowired
 	private CardsRepository cardsRepository;
 	
+	@Autowired
+	private CardsServiceConfig cardsConfig;
+	
 	@PostMapping("/myCards")
 	public List<Cards> getCardDetail(@RequestBody Customer customer) {
 		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
 
 		return cards;
+	}
+	
+	@GetMapping("/cards/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+				cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+		String result = ow.writeValueAsString(properties);
+		return result;
 	}
 }
