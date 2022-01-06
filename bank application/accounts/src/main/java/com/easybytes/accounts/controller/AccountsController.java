018ab1498edbddaba3b8ae3091eafd70d5093c85
Yevhen Strazhko <strazhkoe.dev@gmail.com>
2022-01-06 19:26:27 +0200

    Implemented rate limiter pattern

--- bank application/accounts/src/main/java/com/easybytes/accounts/controller/AccountsController.java
+++ bank application/accounts/src/main/java/com/easybytes/accounts/controller/AccountsController.java
@@ -23,6 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -76,6 +77,12 @@ public class AccountsController {
 		return customerDetails;
 	}
 	
+	@GetMapping("/sayHello")
+	@RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallback")
+	public String sayHello() {
+		return "Hello!";
+	}
+	
 	private CustomerDetails myCustomerDetailFallBack(Customer customer, Throwable t) {
 		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
 		List<Loans> loansDetails = loansFeignClient.getLoansDetails(customer);
@@ -85,6 +92,10 @@ public class AccountsController {
 		customerDetails.setLoans(loansDetails);
 		
 		return customerDetails;
+	}
+	
+	private String sayHelloFallback(Throwable t) {
+		return "Say Hello Falling back";
 	}
 
 }
