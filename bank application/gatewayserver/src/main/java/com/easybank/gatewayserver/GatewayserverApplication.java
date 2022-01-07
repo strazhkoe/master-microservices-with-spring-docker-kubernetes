821faf091f7bfb94125b862f498c965aad28a07f
Yevhen Strazhko <strazhkoe.dev@gmail.com>
2022-01-07 15:52:05 +0200

    Added default setup of the gateway server

--- bank application/gatewayserver/src/main/java/com/easybank/gatewayserver/GatewayserverApplication.java
+++ bank application/gatewayserver/src/main/java/com/easybank/gatewayserver/GatewayserverApplication.java
@@ -2,8 +2,10 @@ package com.easybank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
